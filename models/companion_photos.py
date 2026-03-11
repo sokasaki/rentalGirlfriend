fc5fc91cc73952bc43b84747b9ced844b4176c2d
seth <nolpiseth6666@gmail.com>
2026-03-11 21:35:26 +0700

    Cleanup: Exclude migrations and models from repository

--- models/companion_photos.py
+++
@@ -1,8 +0,0 @@
-from extensions import db
-
-class CompanionPhoto(db.Model):
-    __tablename__ = 'companion_photos'
-    photo_id = db.Column(db.Integer, primary_key=True)
-    companion_id = db.Column(db.Integer, db.ForeignKey('companion_profiles.companion_id'), nullable=False)
-    photo_url = db.Column(db.String(255), nullable=False)
-    is_primary = db.Column(db.Boolean, default=False)
--- models/favorites.py
+++
@@ -1,9 +0,0 @@
-from extensions import db
-from datetime import datetime
-
-class Favorite(db.Model):
-    __tablename__ = 'favorites'
-    favorite_id = db.Column(db.Integer, primary_key=True)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customer_profiles.customer_id'), nullable=False)
-    companion_id = db.Column(db.Integer, db.ForeignKey('companion_profiles.companion_id'), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
